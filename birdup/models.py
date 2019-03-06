be4338d7bb1b201e67a18d282eaa5a4c6827863b
Quinn Dougherty <quinndougherty92@gmail.com>
2019-03-06 18:53:07 -0500

    wednesday

--- birdup/models.py
+++ birdup/models.py
@@ -6,8 +6,9 @@ DB = SQLAlchemy()
 
 class User(DB.Model):
     ''' twitter users that we pull and analyze tweets for. '''
-    id = DB.Column(DB.Integer, primary_key=True)
+    id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
         return f'<User {self.name}>'
@@ -15,10 +16,18 @@ class User(DB.Model):
 
 class Tweet(DB.Model):
     ''' tweets '''
-    id = DB.Column(DB.Integer, primary_key=True)
-    text = DB.Column(DB.Unicode(280))
-    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(500))
+    embedding = DB.Column(DB.PickleType, nullable=False)
+    user_id = DB.Column(
+        DB.BigInteger,
+        DB.ForeignKey('user.id'),
+        nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
     def __repr__(self):
-        return f'<Tweet {self.text}>'
+        try:
+            x = self.text
+        except BaseException:
+            x = self.full_text
+        return f'<Tweet {x}>'
